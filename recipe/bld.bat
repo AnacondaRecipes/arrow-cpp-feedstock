4462e295bf1583bba96b017e54000e28881e3977
Kai Tietz <ktietz@anaconda.com>
2021-02-10 13:37:52 +0100

    add dataset, aws-sdk, and some additional features for 3.0.0 version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 mkdir "%SRC_DIR%"\cpp\build
 pushd "%SRC_DIR%"\cpp\build
 
+rem we don't support CUDA for now
+set "EXTRA_CMAKE_ARGS=-DARROW_CUDA=OFF"
+
 set BOOST_ROOT="%LIBRARY_PREFIX%"
 set Boost_ROOT="%LIBRARY_PREFIX%"
 cmake -G "Ninja" ^
@@ -14,19 +17,23 @@ cmake -G "Ninja" ^
       -DARROW_BUILD_TESTS:BOOL=OFF ^
       -DARROW_BUILD_UTILITIES:BOOL=OFF ^
       -DARROW_BUILD_STATIC:BOOL=OFF ^
-      -DCMAKE_BUILD_TYPE=release ^
       -DARROW_SSE42:BOOL=OFF ^
       -DARROW_PYTHON:BOOL=ON ^
+      -DARROW_MIMALLOC:BOOL=OFF ^
+      -DARROW_DATASET:BOOL=ON ^
       -DARROW_FLIGHT:BOOL=ON ^
+      -DARROW_HDFS:BOOL=ON ^
       -DARROW_PARQUET:BOOL=ON ^
       -DARROW_GANDIVA:BOOL=OFF ^
       -DARROW_ORC:BOOL=ON ^
+      -DARROW_S3:BOOL=ON ^
       -DARROW_WITH_BZ2:BOOL=ON ^
       -DARROW_WITH_ZLIB:BOOL=ON ^
-      -DARROR_WITH_ZSTD:BOOL=ON ^
+      -DARROW_WITH_ZSTD:BOOL=ON ^
       -DBoost_NO_BOOST_CMAKE=ON ^
       -DCMAKE_C_FLAGS="%CFLAGS% -D_WIN32_WINNT=0x600" ^
       -DCMAKE_CXX_FLAGS="%CXXFLAGS% -D_WIN32_WINNT=0x600" ^
+      -DCMAKE_UNITY_BUILD=ON ^
       ..
 
 cmake --build . --target install --config Release
