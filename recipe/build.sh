b3a4ebc17350cc18f8cb524237876639a31c1acc
Tobias "Tobi" Koch <tkoch@anaconda.com>
2022-06-23 11:20:50 +0300

    Disable dark magic

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,25 +9,25 @@ pushd cpp/build
 EXTRA_CMAKE_ARGS=""
 
 # Include g++'s system headers
-if [ "$(uname)" == "Linux" ]; then
-  SYSTEM_INCLUDES=$(echo | ${CXX} -E -Wp,-v -xc++ - 2>&1 | grep '^ ' | awk '{print "-isystem;" substr($1, 1)}' | tr '\n' ';')
-  EXTRA_CMAKE_ARGS=" -DARROW_GANDIVA_PC_CXX_FLAGS=${SYSTEM_INCLUDES}"
-fi
+#if [ "$(uname)" == "Linux" ]; then
+#  SYSTEM_INCLUDES=$(echo | ${CXX} -E -Wp,-v -xc++ - 2>&1 | grep '^ ' | awk '{print "-isystem;" substr($1, 1)}' | tr '\n' ';')
+#  EXTRA_CMAKE_ARGS=" -DARROW_GANDIVA_PC_CXX_FLAGS=${SYSTEM_INCLUDES}"
+#fi
 
 # we don't provide right now CUDA variant
-EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_CUDA=OFF"
+EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_CUDA=OFF DARROW_GANDIVA=OFF"
 
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-    # We need llvm 11+ support in Arrow for this
-    EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_GANDIVA=OFF"
-    sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../src/arrow/flight/CMakeLists.txt
-    sed -ie 's;"--with-jemalloc-prefix\=je_arrow_";"--with-jemalloc-prefix\=je_arrow_" "--with-lg-page\=14";g' ../cmake_modules/ThirdpartyToolchain.cmake
-else
-    EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_GANDIVA=OFF"
-fi
+#if [[ "${target_platform}" == "osx-arm64" ]]; then
+#    # We need llvm 11+ support in Arrow for this
+#    EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_GANDIVA=OFF"
+#    sed -ie "s;protoc-gen-grpc.*$;protoc-gen-grpc=${BUILD_PREFIX}/bin/grpc_cpp_plugin\";g" ../src/arrow/flight/CMakeLists.txt
+#    sed -ie 's;"--with-jemalloc-prefix\=je_arrow_";"--with-jemalloc-prefix\=je_arrow_" "--with-lg-page\=14";g' ../cmake_modules/ThirdpartyToolchain.cmake
+#else
+#    EXTRA_CMAKE_ARGS=" ${EXTRA_CMAKE_ARGS} -DARROW_GANDIVA=OFF"
+#fi
 
-export BOOST_ROOT="${LIBRARY_PREFIX}"
-export Boost_ROOT="${LIBRARY_PREFIX}"
+#export BOOST_ROOT="${LIBRARY_PREFIX}"
+#export Boost_ROOT="${LIBRARY_PREFIX}"
 
 cmake \
     -DARROW_BOOST_USE_SHARED=ON \
@@ -70,13 +70,13 @@ cmake \
     ${EXTRA_CMAKE_ARGS} \
     ..
 
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-    ln -s $BUILD_PREFIX/bin/$HOST-ar $HOST-ar
-    ln -s $BUILD_PREFIX/bin/$HOST-ranlib $HOST-ranlib
-    ninja jemalloc_ep-prefix/src/jemalloc_ep-stamp/jemalloc_ep-patch mimalloc_ep-prefix/src/mimalloc_ep-stamp/mimalloc_ep-patch
-    cp $BUILD_PREFIX/share/gnuconfig/config.* jemalloc_ep-prefix/src/jemalloc_ep/build-aux/
-    sed -ie 's/list(APPEND mi_cflags -march=native)//g' mimalloc_ep-prefix/src/mimalloc_ep/CMakeLists.txt
-fi
+#if [[ "${target_platform}" == "osx-arm64" ]]; then
+#    ln -s $BUILD_PREFIX/bin/$HOST-ar $HOST-ar
+#    ln -s $BUILD_PREFIX/bin/$HOST-ranlib $HOST-ranlib
+#    ninja jemalloc_ep-prefix/src/jemalloc_ep-stamp/jemalloc_ep-patch mimalloc_ep-prefix/src/mimalloc_ep-stamp/mimalloc_ep-patch
+#    cp $BUILD_PREFIX/share/gnuconfig/config.* jemalloc_ep-prefix/src/jemalloc_ep/build-aux/
+#    sed -ie 's/list(APPEND mi_cflags -march=native)//g' mimalloc_ep-prefix/src/mimalloc_ep/CMakeLists.txt
+#fi
 
 ninja install
 
